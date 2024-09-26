326eacc4b4c0d31ab1bc40a52dc5b000c1692762
Mani0818 <mani.bas08@gmail.com>
2024-09-27 01:53:24 +0530

    Update AddressValidator.py

--- AddressValidator.py
+++ AddressValidator.py
@@ -3,7 +3,7 @@ name: Deploy to EC2
 on:
   push:
     branches:
-      - main  # Change this to your main branch name if it's different
+      - main 
 
 jobs:
   deploy:
@@ -19,8 +19,9 @@ jobs:
         ssh-private-key: ${{ secrets.EC2_SSH_KEY }}
 
     - name: Copy files to EC2
-      run: |
-        scp -o StrictHostKeyChecking=no -r ./path/to/your/files ${USERNAME}@${{ secrets.EC2_INSTANCE_IP }}:/path/on/ec2
+  run: |
+    scp -o StrictHostKeyChecking=no -r AddressValidator.py ${USERNAME}@${{ secrets.EC2_INSTANCE_IP }}:/home/ec2-user/
+
 
     - name: Execute commands on EC2
       run: |
