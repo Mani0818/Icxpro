2ff6bc178259c4a2e03dc44b7e7761a19a1f2fcf
Mani0818 <mani.bas08@gmail.com>
2024-09-27 01:19:27 +0530

    Update AddressValidator.py

--- AddressValidator.py
+++ AddressValidator.py
@@ -1,17 +1,27 @@
-def addressVal(address):
-    dot = address.find(".")
-    at = address.find("@")
-    if (dot == -1):
-        print("Invalid")
-    elif (at == -1):
-        print("Invalid")
-    else:
-        
-        print("Valid")
+name: Deploy to EC2
 
-print("This program will decide if your input is a valid email address")
-while(True):
-    print("A valid email address needs an '@' symbol and a '.'")
-    x = input("Input your email address:")
+on:
+  push:
+    branches:
+      - main  # Change this to your main branch name if it's different
 
-    addressVal(x)
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Checkout code
+      uses: actions/checkout@v2
+
+    - name: Set up SSH
+      uses: webfactory/ssh-agent@v0.5.3
+      with:
+        ssh-private-key: ${{ secrets.EC2_SSH_KEY }}
+
+    - name: Copy files to EC2
+      run: |
+        scp -o StrictHostKeyChecking=no -r ./path/to/your/files ${USERNAME}@${{ secrets.EC2_INSTANCE_IP }}:/path/on/ec2
+
+    - name: Execute commands on EC2
+      run: |
+        ssh -o StrictHostKeyChecking=no ${USERNAME}@${{ secrets.EC2_INSTANCE_IP }} 'bash -s' < ./path/to/your/deployment/script.sh
